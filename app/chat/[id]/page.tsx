039040970422091035176791ea7ea3baf027aec8
innbits2017 <innbits@gmail.com>
2026-06-21 01:10:36 +0530

    chat

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -26,7 +26,6 @@ export default async function ChatPage({
 
 return (
   <div className="p-6">
-    <RealtimeMessages />
     <h1 className="text-2xl font-bold mb-6">
       {contact.name || contact.phone}
     </h1>
@@ -46,6 +45,7 @@ return (
       ))}
     </div>
 
+    <MessageComposer contactId={contact.id} />
   </div>
 );
 }
